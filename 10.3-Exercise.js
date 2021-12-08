685237d80460c38487fe13fc371afc297d409a06
JHoopexe <79650306+JHoopexe@users.noreply.github.com>
2021-12-08 09:49:29 -0500

    Add files via upload

---
+++ 10.3-Exercise.js
@@ -0,0 +1,9 @@
+function double(arr){
+    return arr.map(val => val * 2);
+}
+
+function squareAndFindEvens(numbers){
+    let squares = numbers.map(num => num ** 2);
+    let evens = squares.filter(square => square % 2 === 0);
+    return evens;
+}
